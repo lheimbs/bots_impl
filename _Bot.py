1815e61fa55fa4513b47934ea4d7ae0c975e6bb3
lheimbs <l.heimbs@gmail.com>
2019-06-20 22:32:50 +0200

    Ad _Bot.py file with outsources Game methods

---
+++ _Bot.py
@@ -0,0 +1,53 @@
+class Mixin:
+    def training(self):
+        #----Training Game Mode----
+        import training
+        if not self.get_view():
+            return 'q'
+        cmd = training.getch()
+
+        if cmd == 'w':
+            cmd = '^'
+        elif cmd == 's':
+            cmd = 'v'
+        elif cmd == 'a':
+            cmd = '<'
+        elif cmd == 'd':
+            cmd = '>'
+        return cmd
+
+    def escape(self):
+        #----Escape Game Mode----
+        pass
+
+    def collect(self):
+        #----Collect Game Mode----
+        pass
+
+    def snakes(self):
+        #----Snakes Game Mode----
+        pass
+
+    def rumble(self):
+        #----Rumble Game Mode----
+        pass
+
+    def avoid(self):
+        #----Avoid Game Mode----
+        pass
+
+    def word(self):
+        #----Word Game Mode----
+        pass
+
+    def boom(self):
+        #----Boom Game Mode----
+        pass
+
+    def horde(self):
+        #----Horde Game Mode----
+        pass
+
+    def dig(self):
+        #----Dig Game Mode----
+        pass
