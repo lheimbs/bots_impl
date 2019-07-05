35b22968640d249c327699074f28880c2b7c4030
lheimbs <l.heimbs@gmail.com>
2019-07-05 10:48:10 +0200

    Fix bug when hitting obstacle next turn
    Use old_view instead of view when checking for collisions
    Write Map2 in Map file

--- improved/Map.py
+++ improved/Map.py
@@ -1,220 +1,163 @@
 import numpy
-
-""" TODO: rotation der map fixen
-    TODO: map out of bounds weiter führen
-    TODO: logstring wieder entfernen
-"""
+import Map_Help
 
 class Map():
-    def __init__(self, screen, size, fov):
-        self.screen = screen
+    def __init__(self, screen, size, fov, player_collision=False):
+        self._screen = screen
         self._size = size
         self._fov = fov
         self._map = numpy.zeros((size,size), dtype='<U1')
         self._map = numpy.char.replace(self._map, "", "-")
-        self._orientation = self.Orientation()
-        self._coords = self.Coordinates(size, fov)
-        self.old_view = None
-        self.current_view = None
-        self.logstring = ""
+        self._old_view = None
+        self._current_view = None
+        self.orientation = Map_Help.Orientation()
+        self.coords = Map_Help.Coordinates(size, fov)
+        self.player_collision = player_collision
 
-    def debug_view(self, view):
-        # current view:
-        rows, cols = view.shape
-        y = 2+self._fov+3
-        x = self._size + 5
-        self.screen.addstr(y, x, "Current View:")
-        for row in range(rows):
-            for col in range(cols):
-                try: self.screen.addstr(row+y+1, col+1+x, view[row, col])
-                except: pass
-        if self.old_view is not None:
-            rows, cols = self.old_view.shape
-            y = 2
-            x = self._size + 5
-            self.screen.addstr(y, x, "Old View:")
-            for row in range(rows):
-                for col in range(cols):
-                    try: self.screen.addstr(row+y+1, col+1+x, self.old_view[row, col])
-                    except: pass
+    def update(self, view, command, turn):
+        self._current_view = view
+        
 
-    def update(self, view, last_command, turn):
+        if command == '<':
+            # change orientation: rotate anti-clockwise
+            self.orientation.turn_west()
+        elif command == '>':
+            # change orientation: rotate clockwise
+            self.orientation.turn_east()
+        elif command == '^' and not self.is_hit_obstacle(view, command):
+            # move one step forward in current direction
+            self.move_forward()
+        elif command == 'v' and not self.is_hit_obstacle(view, command):
+            # move one step backwards in current direction
+            self.move_backward()
+        else:
+            pass
+
+        view = self.rotate_map(view)
+            
         self.debug_view(view)
+        self._old_view = self._current_view  
+        self._map = self.coords.set_map(view, self._map)
+        return False
 
-        if self.old_view is not None and numpy.array_equal(view, self.old_view):
-            self.logstring += "same view; "
-            # same view as before; and check if way is blocked -> no movement
-            # or way is free -> map just has not changed (empty map or sth)
-            if  self.is_hit_obstacle(view) and last_command in "^v":
-                self.logstring += "hit obstacle; "
-                return
+    def rotate_map(self, view):
+        if self.orientation.ori == "north":
+            view = numpy.char.replace(view, 'A', '▲')
+        elif self.orientation.ori == "west":
+            view = self.rotate_west(view)
+        elif self.orientation.ori == "south":
+            view = self.rotate_south(view)
+        elif self.orientation.ori == "east":
+            view = self.rotate_east(view)
+        return view
 
-        self.old_view = view
-
-        if last_command == "<":
-            self.logstring += "last cmd <; ori: " + self._orientation.ori + "; "
-            # going forwards
-            self._orientation.turn_west()
-            self.view = self.rotate_west(view)
-        elif last_command == ">":
-            self.logstring += "last cmd >; ori: " + self._orientation.ori + "; "
-            self._orientation.turn_east()
-            self.view = self.rotate_east(view)
-        
-        elif last_command == "^":
-            self.logstring += "last cmd ^; "
-            if self._orientation.ori == "north":
-                self.logstring += "move north; "
-                self._coords.move_north()
-            elif self._orientation.ori == "south":
-                self.logstring += "move south; "
-                self._coords.move_south()
-            elif self._orientation.ori == "west":
-                self.logstring += "move west; "
-                self._coords.move_west()
-            elif self._orientation.ori == "east":
-                self.logstring += "move east; "
-                self._coords.move_east()
-
-            self._map[self._coords.get_rows(), self._coords.get_cols()] = view
-
-        elif last_command == "v":
-            self.logstring += "last cmd v; "
-            if self._orientation.ori == "north":
-                self.logstring += "move south; "
-                self._coords.move_south()
-            elif self._orientation.ori == "south":
-                self.logstring += "move north; "
-                self._coords.move_north()
-            elif self._orientation.ori == "west":
-                self.logstring += "move east; "
-                self._coords.move_east()
-            elif self._orientation.ori == "east":
-                self.logstring += "move west; "
-                self._coords.move_west()
-
-            self._map[self._coords.get_rows(), self._coords.get_cols()] = view
-        
-        if turn == 0:
-            self.logstring += "init show map; "
-            self._map[self._coords.get_rows(), self._coords.get_cols()] = view
-
-    def rotate_west(self, view):
-        self.logstring += "rotate west; "
-        # rotate 270° array anti-clockwise once and turn the other paylers characters as well:
-        # > to ^
-        # ^ to <
-        # < to v
-        # v to >
-        view = numpy.rot90(view, 3)
-        view = numpy.char.replace(view, '>', 'a')
-        view = numpy.char.replace(view, '^', 'b')
-        view = numpy.char.replace(view, 'v', 'd')
-        view = numpy.char.replace(view, '<', 'e')
-        view = numpy.char.replace(view, 'a', '^')
-        view = numpy.char.replace(view, 'b', '<')
-        view = numpy.char.replace(view, 'd', '>')
-        view = numpy.char.replace(view, 'e', 'v')
-
-        view = numpy.char.replace(view, 'A', 'c')
+    def rotate_south(self, view):
+        view = numpy.rot90(view, 2) #rotate 180°
+        view = numpy.char.replace(view, 'A', '▼')
         return view
 
     def rotate_east(self, view):
-        self.logstring += "rotate east; "
-        # rotate 90° array anti-clockwise once and turn the other paylers characters as well:
-        # > to v
-        # ^ to >
-        # < to ^
-        # v to <
-        view = numpy.rot90(view, 1)
-        view = numpy.char.replace(view, '<', 'a')
-        view = numpy.char.replace(view, 'v', 'b')
-        view = numpy.char.replace(view, '>', 'c')
-        view = numpy.char.replace(view, '^', 'd')
-        view = numpy.char.replace(view, 'a', '^')
-        view = numpy.char.replace(view, 'b', '<')
-        view = numpy.char.replace(view, 'c', 'v')
-        view = numpy.char.replace(view, 'd', '>')
+        view = numpy.rot90(view, 3) #rotate 180°
+        view = numpy.char.replace(view, 'A', '▶')
         return view
 
-    def is_hit_obstacle(self, view, player_collision = False):
+    def rotate_west(self, view):
+        view = numpy.rot90(view, 1) #rotate 90° anti-clockwise
+        view = numpy.char.replace(view, 'A', '◀')
+        return view
+
+    def move_forward(self):
+        if self.orientation.ori == "north":
+            self.coords.move_north()
+        elif self.orientation.ori == "west":
+            self.coords.move_west()
+        elif self.orientation.ori == "south":
+            self.coords.move_south()
+        elif self.orientation.ori == "east":
+            self.coords.move_east()
+        else:
+            pass
+
+    def move_backward(self):
+        if self.orientation.ori == "north":
+            self.coords.move_south()
+        elif self.orientation.ori == "west":
+            self.coords.move_east()
+        elif self.orientation.ori == "south":
+            self.coords.move_north()
+        elif self.orientation.ori == "east":
+            self.coords.move_west()
+        else:
+            pass
+
+    def is_hit_obstacle(self, view, command):
         obstacle = "#~X"
-        if player_collision:
+        if self.player_collision:
             obstacle += "^v<>"
 
-        if view[int(self._fov/2)-1, int(self._fov/2)] in obstacle:
+        if self._old_view[int(self._fov/2)-1, int(self._fov/2)] in obstacle and command == '^':
+            # is obstacle in front
             return True
-        elif view[int(self._fov/2)+1, int(self._fov/2)] in obstacle:
+        elif self._old_view[int(self._fov/2)+1, int(self._fov/2)] in obstacle and command == 'v':
+            # is obstacle underneath
             return True
         else:
             return False
+
+    def debug_view(self, rotated_view):
+        #debug string
+        self._screen.addstr(1,1, "Orientation: %s Coordinate: %d : %d" % 
+        (self.orientation.ori, self.coords._x, self.coords._y))
+
+        # old view up top
+        if self._old_view is not None:
+            rows, cols = self._old_view.shape
+            y = 2
+            x = self._size + 5
+            self._screen.addstr(y, x, "Old View:")
+            for row in range(rows):
+                for col in range(cols):
+                    try: self._screen.addstr(row+y+1, col+1+x, self._old_view[row, col])
+                    except: pass
+
+        # current view recieved from server middle
+        rows, cols = self._current_view.shape
+        y = 2
+        x = self._size + 2*5 + self._fov
+        self._screen.addstr(y, x, "Current View:")
+        for row in range(rows):
+            for col in range(cols):
+                try: self._screen.addstr(row+y+1, col+1+x, self._current_view[row, col])
+                except: pass
+
+        # new rotated view bottom
+        if rotated_view is not None:
+            rows, cols = rotated_view.shape
+        y = 2
+        x = self._size + 3*5 + 2*self._fov
+        self._screen.addstr(y, x, "Rotated View:")
+        for row in range(rows):
+            for col in range(cols):
+                try: self._screen.addstr(row+y+1, col+1+x, rotated_view[row, col])
+                except: pass
 
     def print(self, title):
         x = 3
         #self.screen.clear()
         #height, width = screen.getmaxyx()
-        self.screen.addstr(0, 5, title)
-        self.screen.addstr(1, 0, self.logstring)
-        self.logstring = ""
+        self._screen.addstr(0, 5, title)
         
         rows, cols = self._map.shape
         try:
-            self.screen.addstr(2, 0, "_" * cols+2*"_")
-            self.screen.addstr(rows+x, 0, "‾" * cols+2*"‾")
+            self._screen.addstr(2, 0, "_" * cols+2*"_")
+            self._screen.addstr(rows+x, 0, "‾" * cols+2*"‾")
         except: pass
         for row in range(rows):
             try: 
-                self.screen.addstr(row+x, 0, "|")
-                self.screen.addstr(row+x, cols+1, "|")
+                self._screen.addstr(row+x, 0, "|")
+                self._screen.addstr(row+x, cols+1, "|")
             except: pass
             for col in range(cols):
-                try: self.screen.addstr(row+x, col+1, self._map[row, col])
+                try: self._screen.addstr(row+x, col+1, self._map[row, col])
                 except: pass
-        self.screen.refresh()
-
-    class Coordinates():
-        def __init__(self, size, fov):
-            self.rows = [size-fov, size]
-            self.cols = [size-fov, size]
-
-        def move_north(self):
-            self.rows = [x-1 for x in self.rows]
-
-        def move_south(self):
-            self.rows = [x+1 for x in self.rows]
-
-        def move_west(self):
-            self.cols = [y-1 for y in self.cols]
-
-        def move_east(self):
-            self.cols = [y+1 for y in self.cols]
-
-        def get_rows(self):
-            return slice(self.rows[0], self.rows[1])
-
-        def get_cols(self):
-            return slice(self.cols[0], self.cols[1])
-    
-    class Orientation():
-        def __init__(self):
-            self.ori = "north"
-        
-        def turn_east(self):
-            if self.ori == "north":
-                self.ori = "east"
-            elif self.ori == "east":
-                self.ori = "south"
-            elif self.ori == "south":
-                self.ori = "west"
-            else:
-                self.ori = "north"
-
-        def turn_west(self):
-            if self.ori == "north":
-                self.ori = "west"
-            elif self.ori == "west":
-                self.ori = "south"
-            elif self.ori == "south":
-                self.ori = "east"
-            else:
-                self.ori = "north"
+        self._screen.refresh()
